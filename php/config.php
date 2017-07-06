6426ccf20d9c35bb7ace8a9cf81c898158560236
Moi <moi.ses.9319@gmail.com>
2017-07-06 15:27:43 +0100

    modificando config.php

--- php/config.php
+++ php/config.php
@@ -5,14 +5,5 @@
 		
 		//PHP Variables
 		const PATH			= "/var/www/html/TFG";
-
-		// Services
-		const SERVICIO_ADD_POST		= "";
-		const SERVICIO_EDIT_POST	= "";
-		const SERVICIO_ADD_COMMENT	= "";
-		const SERVICIO_EDIT_COMMENT	= "";
-		const SERVICIO_DELETE_POST	= "";
-		const SERVICIO_DELETE_COMMENT	= "";
-		const SERVICIO_RELOAD_POSTS	= "";
 	}
 ?>
